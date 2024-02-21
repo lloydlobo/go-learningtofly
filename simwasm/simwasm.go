6cd71feba78166f9e00db0a924267f17716c3a41
Lloyd Lobo <lloydlobo4@gmail.com>
2024-02-21 15:44:09 +0530

    feat: create frontend www and simwasm WebAssembly-aware wrapper wip

--- simwasm/simwasm.go
+++ simwasm/simwasm.go
@@ -1,11 +1,31 @@
 package simwasm
 
-import "math/rand"
+import (
+	"math/rand"
 
-// FUTURE: use lib_simulation as sim;
+	"simcore"
+)
 
 // #[wasm_bindgen]
 type Simulation struct {
-	Rng rand.Rand
-	// sim sim.Simulation
+	Rng *rand.Rand
+	Sim simcore.Simulation
 }
+
+// #[wasm_bindgen]
+// impl Simulation {
+// ...
+
+// #[wasm_bindgen(constructor)]
+func New() *Simulation {
+	// Original rand from Rust compile error:
+	//  - rand internally depends on getrandom, which does support WebAssembly
+	//    inside a web browser, but only when asked explicitly:
+	rng := rand.New(rand.NewSource(0))
+	sim := (&simcore.Simulation{}).Random(rng)
+
+	return &Simulation{rng, sim}
+}
+
+// ...
+// } // end of impl Simulation
---
+++ www/index.js
@@ -0,0 +1,8 @@
+// FIXME: unimplemented
+import * as simwasm from "simwasm";
+
+const simulation = new simwasm.Simulation();
+// --------------- ^-^
+// | For all practical purposes, this is a fancy syntax for Rust's:
+// | `Simulation::new()`
+// ---
