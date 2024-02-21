1a1e47b6fa3f1f75f5aeaed6ebb3e6b0aefd5fd5
Lloyd Lobo <lloydlobo4@gmail.com>
2024-02-21 17:03:52 +0530

    feat: implement from traits to convert core to wasm

--- simwasm/simwasm.go
+++ simwasm/simwasm.go
@@ -12,9 +12,9 @@ type Simulation struct {
 	Sim simcore.Simulation
 }
 
-// #[wasm_bindgen]
-// impl Simulation {
-// ...
+// | #[wasm_bindgen]
+// | impl Simulation {
+// v ...
 
 // #[wasm_bindgen(constructor)]
 func New() *Simulation {
@@ -27,5 +27,59 @@ func New() *Simulation {
 	return &Simulation{rng, sim}
 }
 
-// ...
-// } // end of impl Simulation
+// World implements returns converted Sim's simcore.World to World.
+func (s *Simulation) World() World {
+	return World{}.FromCoreWorld(&s.Sim.World)
+}
+
+// func (s Simulation) wasm_bindgen() {
+// 	// wasm_bindgen implementation for Simulation goes here
+// }
+// ^
+// | This function is not directly equivalent to the wasm_bindgen macro in Rust.
+// | Instead, it serves as a placeholder for the wasm_bindgen implementation in Go.
+
+// ^ ...
+// | } // end of impl Simulation
+
+// | Note: code below is kinda copy-pasted from lib-simulation(simcore),
+// | but with WebAssembly in mind...
+// v
+
+type World struct {
+	Animals []Animal
+	// 	 ^
+	// 	 |
+	// 	error[E0277]: the trait bound `Box<[Animal]>: IntoWasmAbi` is not
+	//               satisfied
+	//  --> libs/simulation-wasm/src/lib.rs
+	//   |
+	// 3 | #[wasm_bindgen]
+	//   | ^^^^^^^^^^^^^^^ the trait `IntoWasmAbi` is not implemented for
+	//   |                 `Box<[Animal]>`
+}
+
+// FromCoreWorld implements conversion of *simcore.World to World.
+func (World) FromCoreWorld(world *simcore.World) World {
+	animals := make([]Animal, 0)
+
+	for _, a := range world.Animals {
+		animals = append(animals, Animal{}.FromCoreAnimal(&a))
+	}
+
+	return World{animals}
+}
+
+type Animal struct {
+	X float32
+	Y float32
+}
+
+// FromCoreAnimal implements conversion of *simcore.Animal to Animal.
+func (Animal) FromCoreAnimal(animal *simcore.Animal) Animal {
+	return Animal{animal.Position.X, animal.Position.Y}
+}
+
+// ^ This model is smaller than `lib_simulation::Animal` (`simcore.Animal`) -
+// | that's because a bird's position is all we need on the JavaScript's
+// | side at the moment; there's no need to map rest of the fields.
